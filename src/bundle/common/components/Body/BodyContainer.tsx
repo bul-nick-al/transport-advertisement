ba3e32b9a381eb8dac08314311f0b42cc7f1484f
nicholas <bul-nick-al@ya.ru>
2019-02-19 22:00:34 +0300

    Router is placed inside Body component

--- src/bundle/common/components/Body/BodyContainer.tsx
+++ src/bundle/common/components/Body/BodyContainer.tsx
@@ -1,11 +1,37 @@
 import Body from './Body';
 import { connect } from 'react-redux';
+import * as React from 'react';
+
+import routes from '../../../orderflow/routes/orderflow';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { RegistrationContainer } from '../../../registration/components/Registration';
 
 const mapStateToProps = state => ({});
 
 const mapDispatchToProps = dispatch => ({});
 
+class BodyContainer extends React.Component<any, any> {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/">Registration</Link>
+                        </li>
+                        <li>
+                            <Link to="/orderflow">Order</Link>
+                        </li>
+                    </ul>
+                    {routes}
+                    <Route exact path="/" component={RegistrationContainer} />
+                </div>
+            </Router>
+        );
+    }
+}
+
 export default connect(
     mapStateToProps,
     mapDispatchToProps
-)(Body);
+)(BodyContainer);
