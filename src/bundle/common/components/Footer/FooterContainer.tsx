a96cb226c50fffd87edaaf1ce9b8620c80cbbb91
nicholas <bul-nick-al@ya.ru>
2019-02-22 10:34:57 +0300

    add Styled components + example

--- src/bundle/common/components/Footer/FooterContainer.tsx
+++ src/bundle/common/components/Footer/FooterContainer.tsx
@@ -1,11 +1,19 @@
 import Footer from './Footer';
 import { connect } from 'react-redux';
 
+import styled from 'styled-components';
+
 const mapStateToProps = state => ({});
 
 const mapDispatchToProps = dispatch => ({});
 
+const FooterStyled = styled(Footer)`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+`;
+
 export default connect(
     mapStateToProps,
     mapDispatchToProps
-)(Footer);
+)(FooterStyled);
