6d32881a97d8151f8614a7c0bc17d4b7980a177b
nicholas <bul-nick-al@ya.ru>
2019-02-23 10:08:07 +0300

    routes removed, version is now 0.0.9

--- src/bundle/orderflow/routes/orderflow.tsx
+++
@@ -1,7 +0,0 @@
-import { Route } from 'react-router-dom';
-import OrderFlowContainer from '../components/OrderFlow/OrderFlowContainer';
-import * as React from 'react';
-
-export default [
-    <Route key="orderflow" path="/orderflow" component={OrderFlowContainer} />,
-];
